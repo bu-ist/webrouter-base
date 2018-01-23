d598eba3d5565e9be89f062d393434cc9975f0ac
David King <dsmk@imp.bu.edu>
2018-01-23 06:15:01 -0500

    Temporary vars.sh approach

--- files/vars.sh
+++ files/vars.sh
@@ -7,17 +7,6 @@ export LOG_LEVEL=warn
 export LANDSCAPE=test 
 export ROBOTS_TXT=/home/robots.txt.test
 
-#export BACKEND_REDIRECT=www-test.bu.edu
-#export BACKEND_WEB_LEGACY=www-test.bu.edu
-#export BACKEND_W3_CONTENT=www-test.bu.edu
-#export BACKEND_W3_MEDIA=www-test.bu.edu
-#export BACKEND_WP_APP=www-test.bu.edu
-#export BACKEND_WP_CONTENT=www-test.bu.edu
-#export BACKEND_PHPBIN=www-test.bu.edu
-#export BACKEND_DBIN=www-test.bu.edu
-#export BACKEND_DJANGO=www-test.bu.edu
-#export BACKEND_DEGREE_ADVICE=www-test.bu.edu
-
 export BACKEND_REDIRECT=test-backend
 export BACKEND_WEB_LEGACY=test-backend
 export BACKEND_W3_CONTENT=test-backend
@@ -28,3 +17,17 @@ export BACKEND_PHPBIN=test-backend
 export BACKEND_DBIN=test-backend
 export BACKEND_DJANGO=test-backend
 export BACKEND_DEGREE_ADVICE=test-backend
+
+if /bin/true ; then
+  export BACKEND_REDIRECT=www-test.bu.edu
+  export BACKEND_WEB_LEGACY=www-test.bu.edu
+  export BACKEND_W3_CONTENT=www-test.bu.edu
+  export BACKEND_W3_MEDIA=www-test.bu.edu
+  export BACKEND_WP_APP=www-test.bu.edu
+  export BACKEND_WP_CONTENT=www-test.bu.edu
+  export BACKEND_PHPBIN=www-test.bu.edu
+  export BACKEND_DBIN=www-test.bu.edu
+  export BACKEND_DJANGO=www-test.bu.edu
+  export BACKEND_DEGREE_ADVICE=www-test.bu.edu
+fi
+
