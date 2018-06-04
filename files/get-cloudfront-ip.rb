5107f499b37ac683bc1e5a0d07b7dcec93db12ca
David King <dsmk@bu.edu>
2018-06-04 13:32:20 -0400

    Adding production F5 IP address

--- files/get-cloudfront-ip.rb
+++ files/get-cloudfront-ip.rb
@@ -22,6 +22,7 @@ print "\n# Set prod VPC subnet as trusted\nset_real_ip_from 10.58.0.0/22;\n"
 print "\n# Set dsmk POC system as trusted\nset_real_ip_from 128.197.229.139/32 ;\n"
 print "\n# Set lab F5 VIP as trusted\nset_real_ip_from 128.197.226.222/32 ;\n"
 print "\n# Set test F5 VIP as trusted\nset_real_ip_from 128.197.70.9/32 ;\n"
+print "\n# Set production F5 VIP as trusted\nset_real_ip_from 128.197.228.215/32 ;\n"
 print "\n# Set the header we are looking at\nreal_ip_header X-Forwarded-For ;\n"
 print "\n# Ignore all trusted IPs\nreal_ip_recursive on ;\n"
 
